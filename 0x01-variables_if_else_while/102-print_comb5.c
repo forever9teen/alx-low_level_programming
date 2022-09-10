da1400f8a6d49cd849049c4d8f19cc7e4b137a8c
Forever19 <opeloyeruwasiu123@gmail.com>
2022-09-09 17:16:06 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,9 +6,11 @@
 * **/
 int main (void)
 {
+
 	int ch;
 	int n;
 	int m;
+
 	for (ch = 48; ch <= 57; ch++)
 	{
 		for (n = 49; n <= 57; n++)
@@ -29,6 +31,6 @@ int main (void)
 			}
 		}
 	}
-		putchar(10);
-		return(0);
+	putchar(10);
+	return(0);
 }
