e90d1817e8b46e0f69b6ad56ce8e19485a60c41f
Forever19 <opeloyeruwasiu123@gmail.com>
2022-09-11 17:31:46 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main point - Entry point
+ * Return: Always success (0)
+ *  **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
