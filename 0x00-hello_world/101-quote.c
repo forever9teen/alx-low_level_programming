4945af4e4733942d7b87a1bfe51a1a5a48b23641
Forever19 <opeloyeruwasiu123@gmail.com>
2022-09-12 20:10:39 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * * main - Entry Point
  *  * - prints and that piece of art is useful - Dora Korpar, 2015-10-19
- *  * *Retrun: Always 1 (success)
+ *  * *Return: Always 1 (success)
  *  **/
 int main(void)
 {
-	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
+	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
 
