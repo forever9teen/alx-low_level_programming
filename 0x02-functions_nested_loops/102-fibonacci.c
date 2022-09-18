66e2eee6f9fdbb807cf502e8819e71e144db72fc
Forever19 <opeloyeruwasiu123@gmail.com>
2022-09-17 18:07:59 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
- *   * followed by a new line
- *    * Return: Always 0 (Success)
- *    * */
+ *  *main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *   *followed by a new line
+ *  *Return: Always 0 (Success)
+ * **/
 int main(void)
 {
 	long int i, j, k, next;
@@ -19,7 +19,7 @@ int main(void)
 		}
 		else
 		{
-		       	printf("%ld\n", j);
+			printf("%ld\n", j);
 		}
 		next = j + k;
 		j = k;
