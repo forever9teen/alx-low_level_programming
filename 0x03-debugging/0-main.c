b126fbfebb8627b08205cf3e80bfdb6e77a2966b
Forever19 <opeloyeruwasiu123@gmail.com>
2022-09-24 17:06:28 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *  * main - prints positive or negative integer
- * * Return: 0
- *    **/
+*  * main - prints positive or negative integer
+* * Return: 0
+*  **/
 int main(void)
 {
 	int i;
@@ -10,3 +10,4 @@ int main(void)
 
 	positive_or_negative(i);
 	return (0);
+}
