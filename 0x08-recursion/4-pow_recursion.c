ae77f24d1fd1826bc83b28b44382bff468ee7f45
Forever19 <opeloyeruwasiu123@gmail.com>
2022-10-01 18:23:07 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,18 +4,18 @@
  *  *@x: The number to be raised.
  *  *@y: The power.
  *  * Return: The value of x raised to the power of y.
- *  **/
+ * **/
 int _pow_recursion(int x, int y)
 {
 	int result = x;
 
 	if (y < 0)
-		 return (-1);
+		return (-1);
 
-	 else if (y == 0)
-		 return (1);
+	else if (y == 0)
+		return (1);
 
-	 result *= _pow_recursion(x, y - 1);
+	result *= _pow_recursion(x, y - 1);
 
-	 return (result);
+	return (result);
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,7 @@
-Write a function that returns the length of a string.
+Write a function that returns the value of x raised to the power of y.
+
+
+
+Prototype: int _pow_recursion(int x, int y);
+
+If y is lower than 0, the function should return -1
