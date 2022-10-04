d9e80c6265cad985131f6d8c30fec9ae332f0773
Forever19 <opeloyeruwasiu123@gmail.com>
2022-10-03 17:55:08 -0700

    Task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,17 +8,17 @@
  * **/
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/
-int count = 0;
+	/*Declaring variables*/
+	int count = 0;
 
-if (argc > 0)
-{
-/*WHILE - Print each arguments*/ 
-while (count < argc)
-{
-printf("%s\n", argv[count]);
-count ++;
-}
-}
-return (0);
+	if (argc > 0)
+	{
+		/*WHILE - Print each arguments*/
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
 }
