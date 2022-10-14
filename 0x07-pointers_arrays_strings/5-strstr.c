dc6a3922357587ec3b877ca3f46b785cfbf1468f
Forever19 <opeloyeruwasiu123@gmail.com>
2022-10-14 03:31:02 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  *  * _strstr - locates a substring
- *   * @haystack: string in which to check for needle
- *    * @needle: substring to find in haystack
- *      * Return: pointer to beginning of needle in haystack or NULL if no match
+ * * @haystack: string in which to check for needle
+ * * @needle: substring to find in haystack
+ * * Return: pointer to beginning of needle in haystack or NULL if no match
  * **/
 char *_strstr(char *haystack, char *needle)
 {
