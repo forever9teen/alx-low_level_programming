c9ba30b70993d9b7ddd8a3d87d8f0c5419fc9c2e
Forever19 <opeloyeruwasiu123@gmail.com>
2022-10-26 05:44:25 -0700

    Task 2

--- main.h
+++ main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**        
- *  * File: main.h
- *   * Auth: sam tech (samuel birhanu) 
+ * main - header file for protoype 
  *    * description: Header file containing prototypes for all functions 
  *   * used in the 0x06-pointers_arrays_strings directory.          
  *   **/
