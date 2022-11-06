65d417402ba973c5b3dd6d2cae116ddac98c1868
Forever19 <opeloyeruwasiu123@gmail.com>
2022-11-06 01:05:05 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,28 +7,31 @@
  * Otherwise - the address of the new element.
  * **/
 listint_t *add_nodeint_end(listint_t **head, const int n)
-{ 
-	listint_t *new, *last;
+{
+	listint_t *new;
+	listint_t *temp;
 
+	(void)temp;
 
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
 
-
 	new->n = n;
 	new->next = NULL;
-
+	temp = *head;
 	if (*head == NULL)
+	{
 		*head = new;
-
-
+	}
 	else
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
 	}
 
 	return (*head);
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -1,11 +1,7 @@
-Write a function that prints all the elements of a listint_t list.
+Write a function that adds a new node at the end of a listint_t list.
 
 
 
-Prototype: size_t print_listint(const listint_t *h);
+Prototype: listint_t *add_nodeint_end(listint_t **head, const int n);
 
-Return: the number of nodes
-
-Format: see example
-
-You are allowed to use printf
+Return: the address of the new element, or NULL if it failed
