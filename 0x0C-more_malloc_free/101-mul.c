48e1ca567c86a6301e8b9b804b9e5a16b82f2c22
Forever19 <opeloyeruwasiu123@gmail.com>
2022-11-23 21:04:49 -0800

    Advance2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,6 +11,7 @@ int is_digit(char *s)
 
 	int i = 0;
 
+
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -28,6 +29,7 @@ int _strlen(char *s)
 {
 
 	int i = 0;
+
 
 	while (s[i] != '\0')
 	{
@@ -54,6 +56,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -94,4 +97,3 @@ int main(int argc, char *argv[])
 	free(result);
 	return (0);
 }
-
